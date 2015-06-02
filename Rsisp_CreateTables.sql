59be8227a93eb9c8eeaf50ca8840d6624dc2e7b8
inokumaw <inokumaw@gmail.com>
2015-06-02 23:55:21 +0800

    123

--- Rsisp_CreateTables.sql
+++ Rsisp_CreateTables.sql
@@ -1,4 +1,4 @@
-use Rsisp
+use AssessDB
 go
 
 create table Roles
@@ -23,10 +23,11 @@ create table Users
 
 create table Patients
 (
-	ID_Patient		nvarchar(20),
-    PatientName		nvarchar(20) not null,
-	PatientIDCard	nvarchar(20) not null unique,
-	PatientBirthday	date,
+	ID_Patient			nvarchar(20),
+    PatientName			nvarchar(20) not null,
+	PatientIDCard		nvarchar(20) not null unique,
+	PatientBirthday		date,
+	PatientPhotoPath	nvarchar(100)
 
     primary key (ID_Patient)
 )
@@ -83,108 +84,16 @@ create table Assess1
 	ItemScore5		int not null,
 	ItemScore6		int not null,
 	ItemScore7		int not null,
+	ItemScore8		int not null,
+	ItemScore9		int not null,
+	ItemScore10		int not null,
+	ItemScore11		int not null,
+	ItemScore12		int not null,
+	ItemScore13		int not null,
+	ItemScore14		int not null,
+	ItemScore15		int not null,
+	ItemText1		nvarchar(100)
 
     primary key (ID_Assess1),
 	foreign key (ID_User) references Users(ID_User)
 )
-
-create table Assess2
-(
-	ID_Assess2		int identity(20000001, 1), 
-	ID_User			nvarchar(20),
-    ItemScore1		int not null,
-	ItemScore2		int not null,
-
-    primary key (ID_Assess2),
-	foreign key (ID_User) references Users(ID_User)
-)
-
-create table Assess3
-(
-	ID_Assess3		int identity(30000001, 1), 
-	ID_User			nvarchar(20),
-    ItemScore1		int not null,
-	ItemScore2		int not null,
-
-    primary key (ID_Assess3),
-	foreign key (ID_User) references Users(ID_User)
-)
-
-create table Assess4
-(
-	ID_Assess4		int identity(40000001, 1), 
-	ID_User			nvarchar(20),
-    ItemScore1		int not null,
-	ItemScore2		int not null,
-
-    primary key (ID_Assess4),
-	foreign key (ID_User) references Users(ID_User)
-)
-
-create table Assess5
-(
-	ID_Assess5		int identity(50000001, 1), 
-	ID_User			nvarchar(20),
-    ItemScore1		int not null,
-	ItemScore2		int not null,
-
-    primary key (ID_Assess5),
-	foreign key (ID_User) references Users(ID_User)
-)
-
-create table Assess6
-(
-	ID_Assess6		int identity(60000001, 1), 
-	ID_User			nvarchar(20),
-    ItemScore1		int not null,
-	ItemScore2		int not null,
-
-    primary key (ID_Assess6),
-	foreign key (ID_User) references Users(ID_User)
-)
-
-create table Assess7
-(
-	ID_Assess7		int identity(70000001, 1), 
-	ID_User			nvarchar(20),
-    ItemScore1		int not null,
-	ItemScore2		int not null,
-
-    primary key (ID_Assess7),
-	foreign key (ID_User) references Users(ID_User)
-)
-
-create table Assess8
-(
-	ID_Assess8		int identity(80000001, 1), 
-	ID_User			nvarchar(20),
-    ItemScore1		int not null,
-	ItemScore2		int not null,
-
-    primary key (ID_Assess8),
-	foreign key (ID_User) references Users(ID_User)
-)
-
-create table AssessRecords
-(
-	ID_Record		int identity(1, 1), 
-	RecordTime		datetime,
-    ID_Patient		nvarchar(20),
-	ID_Assess1		int,
-	ID_Assess2		int,
-
-    primary key (ID_Record),
-	foreign key (ID_Patient) references Patients(ID_Patient),
-	foreign key (ID_Assess1) references Assess1(ID_Assess1),
-	foreign key (ID_Assess2) references Assess2(ID_Assess2),
-)
-
-select * from Roles
-select * from Users
-select * from Patients
-select * from AssessStyle
-select * from AssessItemStyle
-select * from AssessItemContentStyle
-select * from Assess1
-select * from Assess2
-select * from AssessRecords
